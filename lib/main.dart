97fd4fa11e25748072d8f005fdd43f05cbaab024
Mostafa13elkomy <mostafa.elkomy110@gmail.com>
2023-08-05 00:03:43 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:todo/views/notes_view.dart';
+
+void main() {
+  runApp(const NoteApp());
+}
+
+class NoteApp extends StatelessWidget {
+  const NoteApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(brightness: Brightness.dark),
+      home: NotesView(),
+    );
+  }
+}
+  
+
