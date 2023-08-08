d4313ac6fcc2ebf22ecc1f49db0172ff813c532d
Mostafa13elkomy <mostafa.elkomy110@gmail.com>
2023-08-09 02:29:56 +0300

    add fields to note button sheet

--- lib/views/widgets/add_note_button-sheet.dart
+++ lib/views/widgets/add_note_button-sheet.dart
@@ -1,21 +1,29 @@
 import 'package:flutter/material.dart';
 import 'cutom_text_feild.dart';
+
 class AddNoteButtonSheet extends StatelessWidget {
-    const AddNoteButtonSheet({super.key});
-  
-    @override
-    Widget build(BuildContext context) {
-      return Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 16),
-        child: Column(
-          children: [
-            SizedBox(height: 32,),
-      const CutomTextFeild(),
-      
-      
-          ],
-        ),
-      );
-    }
+  const AddNoteButtonSheet({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16),
+      child: Column(
+        children: const [
+          SizedBox(
+            height: 32,
+          ),
+          CutomTextField(
+            hint: 'title',
+          ),
+          SizedBox(
+            height: 16,),
+          CutomTextField(
+            hint: 'content',
+            maxLines: 5,
+        )
+        ],
+      ),
+    );
   }
-  
+}
--- lib/views/widgets/cutom_text_feild.dart
+++ lib/views/widgets/cutom_text_feild.dart
@@ -1,29 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:todo/consts.dart';
 
-class CutomTextFeild extends StatelessWidget {
-  const CutomTextFeild({super.key});
-
+class CutomTextField extends StatelessWidget {
+  const CutomTextField({super.key, required this.hint,  this.maxLines = 1});
+  final String hint;
+  final int maxLines;
   @override
   Widget build(BuildContext context) {
     return TextField(
+      cursorColor: KPrimaryColor,
+      maxLines: maxLines,
       decoration: InputDecoration(
-        hintText: 'title',
-        hintStyle:const TextStyle(
-          color: KPrimaryColor
-        ),
+          hintText: hint,
+          hintStyle: const TextStyle(color: KPrimaryColor),
           border: buildBordrer(),
           enabledBorder: buildBordrer(),
           focusedBorder: buildBordrer(KPrimaryColor)),
-          
     );
   }
 
   OutlineInputBorder buildBordrer([color]) {
     return OutlineInputBorder(
-          borderRadius: BorderRadius.circular(16),
-          borderSide: BorderSide(
-            color:color ?? Colors.white
-          ));
+        borderRadius: BorderRadius.circular(16),
+        borderSide: BorderSide(color: color ?? Colors.white));
   }
 }
