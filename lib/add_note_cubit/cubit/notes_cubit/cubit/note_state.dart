6e78105e21903ed1e0534f55d0557c6bf981dc4f
Mostafa13elkomy <mostafa.elkomy110@gmail.com>
2023-08-22 02:06:15 +0300

    create notes state cubit

---
+++ lib/add_note_cubit/cubit/notes_cubit/cubit/note_state.dart
@@ -0,0 +1,16 @@
+part of 'note_cubit.dart';
+
+@immutable
+class NoteState {}
+
+class NoteInitial extends NoteState {}
+
+class NoteLoading extends NoteState {}
+
+class NoteSuccess extends NoteState {
+  final List<NoteModel> notes;
+
+  NoteSuccess(this.notes);
+}
+
+class NoteFailure extends NoteState {}
--- lib/views/widgets/add_note_button-sheet.dart
+++ lib/views/widgets/add_note_button-sheet.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 import 'package:todo/add_note_cubit/cubit/add_note_cubit.dart';
 import 'add_note_from.dart';
 
@@ -18,7 +17,7 @@ class _AddNoteButtonSheetState extends State<AddNoteButtonSheet> {
     return BlocProvider(
       create: (context) => AddNoteCubit(),
       child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 16),
+        padding:  EdgeInsets.only(left: 16,right: 16,bottom: MediaQuery.of(context).viewInsets.bottom),
         child: BlocConsumer<AddNoteCubit, AddNoteState>(
           listener: (context, state) {
             if (state is AddNoteLoading) {
